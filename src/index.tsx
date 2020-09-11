8a64a4f3f30604f8f6166eb0a0885b7c61732c1b
rolly <rollrodrig@gmail.com>
2020-09-10 21:13:14 -0500

    added readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.scss';
